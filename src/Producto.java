d5989b3b4251de1f64a57b7a9f6b2391e7915a9c
Mileer leon <mileerdubanleon@gmail.com>
2025-11-13 23:53:21 -0500

    feat: add class the products, view product

---
+++ src/Producto.java
@@ -0,0 +1,26 @@
+public class Producto {
+    // atributos de instancia
+    private int id;
+    private String name;
+    private int age;
+    private int quantity;
+    private double price;
+
+    // constructor
+    public Producto(){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public void MostrarProducto(int id, String name, int age, int quantity, double price){
+        System.out.printf("\nId producto: %d", id);
+        System.out.println("\nNombre: " + name);
+        System.out.printf("\nAño: %d", age);
+        System.out.printf("\nCantidad: %d", quantity);
+        System.out.println("\nPrecio: " + price);
+    }
+
+}
